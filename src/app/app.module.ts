2980462d621af71056d2c45df8c4368cc8a92b61
k.s.rogov <k.s.rogov@tinkoff.ru>
2024-03-20 00:24:21 +0300

    докидываю доделанный модуль

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CourseListSectionComponent } from './components/course-list-section.component';
+import { FooterSectionComponent } from './components/footer-section.component';
+import { HeaderSectionComponent } from './components/header-section.component';
+import { SearchSectionComponent } from './components/search-section.component';
 
 @NgModule({
-  declarations: [AppComponent, CourseListSectionComponent],
+  declarations: [
+    AppComponent,
+    CourseListSectionComponent,
+    FooterSectionComponent,
+    HeaderSectionComponent,
+    SearchSectionComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent, CourseListSectionComponent],
